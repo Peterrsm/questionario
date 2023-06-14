9f55f9db034bebd05507560d59f1bfb475656a03
peterrsm <pedrorsmiranda@gmail.com>
2023-06-14 15:48:46 -0300

    ++ 'orderArray' in Exercise2

--- src/main/java/com/pedromiranda/exercises/Exercise2.java
+++ src/main/java/com/pedromiranda/exercises/Exercise2.java
@@ -1,16 +1,29 @@
 package com.pedromiranda.exercises;
 
-import java.util.Arrays;
-
 public class Exercise2 {
     public static void sumhighest(Integer[] numbers) {
-        Arrays.sort(numbers);
-
-        Integer size = numbers.length - 1;
-        Integer first = numbers[size];
-        Integer second = numbers[size-1];
+        numbers = orderArray(numbers);
+        Integer first = numbers[0];
+        Integer second = numbers[1];
         Integer sum = first + second;
 
         System.out.println(first + " + " + second + " = " + sum);
+    }
+
+    private static Integer[] orderArray(Integer[] array){
+        int size = array.length;
+
+        for(int i = 0; i < size-1; i++){
+            for(int j = 0; j < size-1; j++) {
+                if (array[j] < array[j + 1]) {
+                    int aux = array[j];
+
+                    array[j] = array[j + 1];
+                    array[j + 1] = aux;
+                }
+            }
+        }
+
+        return  array;
     }
 }
